6808546543ecac58ece330cd1a8f0308ebad2ed6
JuanHdz23 <51144863+JuanHdz23@users.noreply.github.com>
2020-11-09 17:58:21 -0700

    Agregar archivos de proyecto.

---
+++ PruebaDelegados/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaDelegados
+{
+    public partial class Form1 : Form
+    {
+        private Form2 form2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+            form2.ValueUpdate += new Delegado(form2_ValueUpdate);
+            form2.Show();
+        }
+
+        private void form2_ValueUpdate(object sender, ValueUpdate e )
+        {
+            label1.Text = e.newValue;
+        }
+    }
+}
---
+++ PruebaDelegados/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaDelegados
+{
+    
+    public partial class Form2 : Form
+    {
+        public event Delegado ValueUpdate;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newValue = textBox1.Text;
+            ValueUpdate valueArgs = new ValueUpdate(newValue);
+            ValueUpdate(this, valueArgs);
+        }
+    }
+}
